c20505b4a103dd1847ec87fe939840ca6f85d356
BuckarooBanzay <BuckarooBanzay@users.noreply.github.com>
2023-02-04 11:08:41 +0100

    init

---
+++ world.mt
@@ -0,0 +1,6 @@
+mod_storage_backend = sqlite3
+auth_backend = sqlite3
+player_backend = sqlite3
+backend = sqlite3
+gameid = nodecore
+world_name = nodecore
---
+++ worldmods/nc_test/init.lua
@@ -0,0 +1,7 @@
+
+mtt.register("nc_test", function(callback)
+    -- TODO
+    callback()
+end)
+
+mtt.validate_nodenames(minetest.get_modpath("nc_test") .. "/nodenames.txt")
